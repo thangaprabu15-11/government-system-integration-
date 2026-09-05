a00795dd5ba05ac6cc40954a19fe2f7389fd50dc
THANGAPRABU <thangaprabu@THANGAPRABUs-MacBook-Air.local>
2026-09-05 13:45:34 +0530

    fix: Enable host and allowedHosts in vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    host: true,
+    allowedHosts: true,
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:5001',
